45f461e1b71c5c4975b5e65cbbf4acf484e163c0
Captaintastyshakes <144308095+Captaintastyshakes@users.noreply.github.com>
2023-12-06 16:40:12 -0700

    big structural changes, to html and javascript. Accomodating changes to not get rejected.

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,11 @@ const editProfileButton = document.querySelector('.profile__edit-button');
 
 const profileCloseModal = document.querySelector('section.profile .modal__exit');
 
-const editProfilePopup = document.querySelector('.modal'); //the basic modal
+const addPhotoCloseModal = document.querySelector('section.cards .add__exit');
+
+const cardPreviewCloseModal = document.querySelector('section.cards .preview__exit');
+
+const editProfilePopup = document.querySelector('.modal'); //the profile modal
 
 /*(4.1)for populating form fields*/
 
@@ -58,9 +62,15 @@ const aboutMeField = document.querySelector('#about_me');
 
 const profileOccupation = document.querySelector('.profile__occupation')
 
+const titleField = document.querySelector('.add__input');
+
+const imageURLField = document.querySelector('#image_url');
+
 /* (4.2)submit button*/
 
-const saveButton = document.querySelector('.modal__box'); //having this target the whole form instead of just the submit button
+const saveButtonProfile = document.querySelector('.modal__box'); //having this target the whole form instead of just the submit button
+
+const saveButtonAdd = document.querySelector('.add__box'); //for the add photos box
 
 /*(4.3)card template*/
 
@@ -72,15 +82,19 @@ const cardImplement = document.querySelector('.cards');
 
 const cardAddButton = document.querySelector('.profile__add-button');
 
-const modalHeader = document.querySelector('.modal__header');
+//const modalHeader = document.querySelector('.modal__header');
+
+const addPhotoPopup = document.querySelector('.add');
 
 //(5.6) card preview modal
 
-const cardPreview = document.querySelector('.modal__preview');
+const cardPreviewPopup = document.querySelector('.preview');
 
-const cardPortrait = document.querySelector('.modal__image');
+const cardPreview = document.querySelector('.preview__main');
 
-const cardSubtitle = document.querySelector('.modal__subtitle');
+const cardPortrait = document.querySelector('.preview__image');
+
+const cardSubtitle = document.querySelector('.preview__subtitle');
 
 /*--functions--*/ //------------------------------------------------------------------------------------------
 
@@ -90,7 +104,7 @@ function openModal(modal) {
     modal.classList.add('modal_opened');
 }
 
-function fillProfileForm() {    
+function fillProfileForm() {
     nameField.value = profileName.textContent;
     aboutMeField.value = profileOccupation.textContent;
     modalHeader.textContent = "Edit profile";
@@ -99,30 +113,30 @@ function fillProfileForm() {
 function handleEditProfileButtonClick() {
     openModal(editProfilePopup);
     fillProfileForm();
-    suppressCardPreview();
+    //suppressCardPreview();
 }
 
 editProfileButton.addEventListener("click", handleEditProfileButtonClick);
 
 // (5.2) opening modal for photo add -----------------------------------------
 
-function changeModalToAdd () {
+/*function changeModalToAdd () {
     modalHeader.textContent = "New Place";
     nameField.value = "";
     nameField.placeholder = "Title";
     aboutMeField.value = "";
     aboutMeField.placeholder = "Image URL";
-}
+}*/
 
-function changeSaveToAdd () { 
+/*function changeSaveToAdd () { 
     saveButton.classList.add('saveForAdd');
-}
+}*/
 
 function handleAddButtonClick() {
-    openModal(editProfilePopup);
-    changeModalToAdd();
-    changeSaveToAdd();
-    suppressCardPreview();
+    openModal(addPhotoPopup);
+    //changeModalToAdd();
+    //changeSaveToAdd();
+    //suppressCardPreview();
 }
 
 cardAddButton.addEventListener("click", handleAddButtonClick);
@@ -133,28 +147,29 @@ function fillCardPreview(event) {
     selectedCard = event.target;
     cardPortrait.src = selectedCard.src;
     cardPortrait.alt = selectedCard.alt;
-    cardSubtitle.textContent = selectedCard.alt;    
+    cardSubtitle.textContent = selectedCard.alt;
 };
 
-function supressOtherModalOpening() {    
+/*function supressOtherModalOpening() {    
     saveButton.setAttribute("style", "visibility: hidden");    
-};
+};*/
 
-function openCardPreview() {    
+//
+/*function openCardPreview() {    
     cardPreview.setAttribute("style", "visibility: visible");
     cardPreview.setAttribute("style", "display: flex");
-};
+};*///re-evaluate
 
 function handlePhotoClick(event) {
-    openModal(editProfilePopup);
+    openModal(cardPreviewPopup);
     fillCardPreview(event);
-    supressOtherModalOpening();
-    openCardPreview();
+    //supressOtherModalOpening();
+    //openCardPreview();
 }
 
-function suppressCardPreview() {
+/*function suppressCardPreview() {
     cardPreview.setAttribute("style", "display: none");
-}
+}*/
 
 /*(4.0)closing modal*///---------------------------------------------------------------
 
@@ -162,26 +177,35 @@ function  closeModal(modal) {
     modal.classList.remove('modal_opened');
 }
 
-function closeEditProfileModal() { //catch-all closing function 
-    closeModal(editProfilePopup);
-    expressOtherModalOpening();
-    dumpCardPreview();
-    saveButton.classList.remove("saveForAdd");
+function closeEditProfileModal() {
+    closeModal(editProfilePopup);    
+}
+
+function closeAddPhotoModal() {
+    closeModal(addPhotoPopup);    
+}
+
+function closeCardPreviewModal() {
+    closeModal(cardPreviewPopup);
 }
 
 profileCloseModal.addEventListener("click", closeEditProfileModal);
 
+profileCloseModal.addEventListener("click", closeAddPhotoModal);
+
+profileCloseModal.addEventListener("click", closeCardPreviewModal);
+
 // (5.6) closing photo modal---------------------------------------------------
 
-function dumpCardPreview() {  
+/*function dumpCardPreview() {
   cardPreview.removeAttribute("style", "visibility: visible");
   cardPreview.setAttribute("style", "display: none");
-}
+}*/
 
-function expressOtherModalOpening() {
+/*function expressOtherModalOpening() {
     saveButton.removeAttribute("style", "display: none");
     saveButton.removeAttribute("style", "visibility: hidden");
-};
+};*/
 
 /*(4.2)saving/submitting for profile modal*/// ----------------------------------------------
 
@@ -190,20 +214,37 @@ function fillProfileInfo() {
     profileOccupation.textContent = aboutMeField.value;
 }
 
-function handleProfileFormSubmit(event) {
+/*function handleProfileFormSubmit(event) {
     event.preventDefault();
-    if (saveButton.classList.contains("saveForAdd")) {
+    /*if (saveButton.classList.contains("saveForAdd")) {
         renderCard(makeNewPhoto());
         initialCards.shift(makeNewPhoto()); //add the new object to the array in case it needs to be nuked
         closeEditProfileModal();
-     } 
-    else { 
+     }
+    //else { 
         fillProfileInfo();
         closeModal(editProfilePopup);
-    }
+    //}
+}*/
+
+function handleProfileFormSubmit(event) {
+    event.preventDefault();
+    fillProfileInfo();
+    closeEditProfileModal();
 }
 
-saveButton.addEventListener("submit", handleProfileFormSubmit);
+function handleAddPhotoSubmit(event) {
+    event.preventDefault();
+    renderCard(makeNewPhoto());
+        initialCards.shift(makeNewPhoto()); //add the new object to the array in case it needs to be nuked
+        closeCardPreviewModal();
+}
+
+saveButtonProfile.addEventListener("submit", handleProfileFormSubmit);
+
+saveButtonAdd.addEventListener("submit", handleAddPhotoSubmit);
+
+
 
 //(5.3) adding photos/submitting with the add photo form---------------------------------------
 
@@ -212,8 +253,8 @@ function makeNewPhoto() {
         name: "",
         link: ""
     };
-    newObject.name = nameField.value;
-    newObject.link = aboutMeField.value;
+    newObject.name = titleField.value;
+    newObject.link = imageURLField.value;
     return newObject;
 }
 
@@ -228,7 +269,7 @@ function getCardElement(data) {
     cardImage.alt = data.name;
     cardTitle = cardElement.querySelector('.cards__title');
     cardTitle.textContent = data.name;
-    cardElement.id = cardImage.alt.slice(0, 4) + "LK";//assigning a generated id, just in case. Also helps with loggin if targets are working.    
+    cardElement.id = cardImage.alt.slice(0, 4) + "LK";//assigning a generated id, just in case. Also helps with logging if targets are working.    
     cardDeleteButton.addEventListener("click", handleCardDelete);
     cardImage.addEventListener("click", handlePhotoClick);    
     cardLikeButton.addEventListener("click", handleLikeButtonClick);
