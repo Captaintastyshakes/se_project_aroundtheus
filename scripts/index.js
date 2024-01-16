319dee5ca6939cd17550fbf6b2855fa15fba979c
Michael Weihmanngit config --global user.name Michael <mikej513@hotmail.com>
2024-01-16 00:35:27 -0700

    Batch of implementations. Still working on both validation and page click.

--- scripts/index.js
+++ scripts/index.js
@@ -92,9 +92,9 @@ const cardSubtitle = document.querySelector('.preview-popup__subtitle');
 
 /*opening edit profile modal*///-------------------------------------
 
-function openModal(modal) {
+/*function openModal(modal) {
     modal.classList.add('modal_opened');
-}
+}*/
 
 function fillProfileForm() {
     nameField.value = profileName.textContent;
@@ -132,9 +132,9 @@ function handlePhotoClick(event) {
 
 /*closing modal*///---------------------------------------------------------------
 
-function  closeModal(modal) {
+/*function  closeModal(modal) {
     modal.classList.remove('modal_opened');
-}
+}*/
 
 closeButtons.forEach((button) => {
     const popup = button.closest(".modal");
@@ -232,52 +232,165 @@ function handleCardDelete(event) {
 
 //variables
 
-const closeOverlays = Array.from(document.querySelectorAll(".modal"));
+
+/*
+   **Needs correcting**
+
+ Please, call it `modals` because you fine all modals here G2G
+
+*/
+//const closeOverlays = Array.from(document.querySelectorAll(".modal"));
+/*change to*/ const modals = Array.from(document.querySelectorAll(".modal"));
 
 const pageOverlay = document.querySelector(".page");
+
 
 const modalBoxes = Array.from(document.querySelectorAll(".modal__box"));
 
 //functions
 
-function handlePageClick(evt) {
-    const bigOverlay = evt.target;
+//1A THIS ONE IS VERY TRICKY/SENSITIVE. THE CHANGES I WANT TO MAKE SEEM TO BREAK THE OPEN MODAL FUNCTION. SEE 1B FOR WHAT I IMAGINE I WANT TO DO BUT WHICH DOESN'T WORK
+/*function handlePageClick(evt) {//prime
+    const bigOverlay = evt.target;    
+/***Needs correcting** Please, use `closeOverlays` here because it's the same modals you need G2G
+     No need to search them again and again
     const subordinateOverlays = Array.from(bigOverlay.querySelectorAll(".modal"));
     subordinateOverlays.forEach((subordinateOverlay) => {
         if (subordinateOverlay.classList.contains("modal_opened")) {
             closeModal(subordinateOverlay);
         };
-    });
-    
-}
+    });    
+}*/
 
-pageOverlay.addEventListener("click", handlePageClick);
+/*1B 
 
-function handleEscapePress(evt) {
+/*function handlePageClick(modals) {
+	modals.forEach((modal) => {
+		if(modal.classList.contains("modal_opened")) {
+			closeModal(modal);
+		};
+}); */ //but see this, for some reason, obstructs my open function; I do not get it.*/
+
+//actually I think I can just wire the close function directly in the event listener for the page.
+
+
+/***Needs correcting*** Please, donâ€™t add listeners to `document` or `window` or `pageOverlay` if you donâ€™t remove them because the listeners always watch any clicks on the page and make checks. 
+Thatâ€™s a waste of resources.*/ //G2G
+
+//pageOverlay.addEventListener("click", handlePageClick);
+//hmm maybe I will add this to the open modal function, same with escape since that has the same critique.
+
+/*function handleEscapePress(evt) {//prime
     const bigOverlay = evt.currentTarget;    
-    const subordinateOverlays = Array.from(bigOverlay.querySelectorAll(".modal"));
+    /***Needs correcting** Please, use `closeOverlays` here because it's the same modals you need
+     No need to search them again and again*/
+    /*const subordinateOverlays = Array.from(bigOverlay.querySelectorAll(".modal"));
     subordinateOverlays.forEach((subordinateOverlay) => {
         if ((evt.keyCode == 27) && (subordinateOverlay.classList.contains("modal_opened"))) {            
             closeModal(subordinateOverlay);
         };
     });
+}*/
+
+/*function handleEscapePress(evt) {
+	modals.forEach((modal) => {
+		if (evt.key == "Escape") { //gonna try w/out the open check, seems redundant.
+			closeModal(modal);
+		};
+	});
+}*/ //refactoring 'cos if I am adding to modal on open I don't need to iterate through
+
+function handleEscapePress(evt) {
+    modals.forEach((modal) => {
+        if (evt.key == "Escape") {
+            closeModal(modal);
+        };
+    });    
 }
 
-document.addEventListener("keydown", handleEscapePress);
+
+//REWRITING OPEN MODAL AND CLOSE MODAL HERE, HAVE ANNOTATED THEM OUT ABOVE AND AM REFACTORING HERE
+
+//open modal
+
+function openModal(modal) {
+    modal.classList.add('modal_opened');
+    //document.addEventListener("click", closeModal(modal));
+    document.addEventListener("keydown", handleEscapePress); //very iffy about this- I need to pass it the event object and the modal as a parameter, will see if this works.
+    //document.addEventListener("keydown", (modal) => handleEscapePress(modal)); keeping this handy in the backpocket in case the above doesn't work
+}
+
+//close modal
+
+function closeModal(modal) {
+    modal.classList.remove('modal_opened');    
+    //pageOverlay.removeEventListener("click", closeModal);
+    document.removeEventListener("keydown", handleEscapePress);
+}
+
+/***Needs correcting*** The check list says: An event listener for closing the popup by pressing Esc is added when the popup is opened, and is removed when it's closed.*/ //G2G
+
+//document.addEventListener("keydown", handleEscapePress); //will add elsewhere, maybe integrate with open and closing functions.
 
 function handleBoxClick(evt) {    
-    evt.stopImmediatePropagation();    
+    evt.stopImmediatePropagation();
 }
 
 modalBoxes.forEach((box) => {
     box.addEventListener("click", handleBoxClick);
 });
 
-closeOverlays.forEach((overlay) => {
-    overlay.addEventListener("click", () => closeModal(overlay));    
-});
+/*closeOverlays.forEach((overlay) => {
+    overlay.addEventListener("click", () => closeModal(overlay));
+});*/
+
+modals.forEach((modal) => {
+	modal.addEventListener("click", () => closeModal(modal));
+}); //may have screwed up the function declaration here but you get the idea- just rewriting the above with the new variable
 
 
 
+//-----------------------------------------------------------------------------------------------------------------------------------------worry about this later, get working first
 
+/***Could be improved** If itâ€™s interesting for you here is how you can combine **overlay** and **close buttons** listeners together:
+```js
+  const popups = document.querySelectorAll('.popup')
+      popups.forEach((popup) => {
+          popup.addEventListener('mousedown', (evt) => {
+              if (evt.target.classList.contains('popup_opened')) {
+                  closePopup(popup)
+              }
+          })
+      })
+```
 
+This is why we ask you to put **`generic`** classes in the common markup. In this case itâ€™s class `popup`. We find all popups in the project and run through them adding an overlay listener for each popup.
+
+And here is another **`magic`** of `generic` css-classes:
+
+```js
+ if (evt.target.classList.contains('popup__close')) {
+                closePopup(popup)
+              }
+```
+
+Here we check if you have clicked a **`cross button`** and then we close the popup.
+Any cross button has a generic css-class `popup__close`.
+And now you can delete a lot of redundant code which handles clicks on cross buttons, searching these cross buttons and so on. You can add 10 popups and the code will be still working fine.
+
+```js
+ const popups = document.querySelectorAll('.popup')
+      popups.forEach((popup) => {
+          popup.addEventListener('mousedown', (evt) => {
+              if (evt.target.classList.contains('popup_opened')) {
+                  closePopup(popup)
+              }
+              if (evt.target.classList.contains('popup__close')) {
+                closePopup(popup)
+              }
+          })
+      })
+
+```
+Please, pay attention that you need to use event `'mousedown'` rather than `click` because there is a bug with the overlay: if you click **inside** a popup and then move your mouse **outside** it and release the button above the overlay then the popup closes but itâ€™s not what we expect.
+*/
